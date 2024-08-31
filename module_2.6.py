c92400adbc7ab8f1e278708282466b8ec30b8df5
Ekaterina Galisheva <masay94@mail.ru>
2024-08-31 15:45:54 +0300

    module_2.6.py

--- module_2.6.py
+++ module_2.6.py
@@ -1,28 +1,21 @@
-import re
-from os import remove
-
 print('Приветствую вас, я алгоритм, который поможет открыть дверь!')
-n = input('Введите первое число от 3 до 20 и я подберу пароль: ')
-n = int(n)
+number = int(input('Введите первое число от 3 до 20 и я подберу пароль: '))
 
 password = []
-#modificate_password = re.sub( ),( )
 
-if (n < 3) and (n > 20): # НЕ РАБОТАЕТ
-    print('Неверное число! Вас раздавили шипы!')
-else:
-    print('Скорее!')
-
-for i in range(1, n):
-    for j in range(i, n):
-        if n % (i + j) == 0:
+for i in range(1, number):
+    for j in range(i, number):
+        if number % (i + j) == 0:
             key = i, j
             password.append(key)
 
-
-
-print( )
-print('Ваш пароль:')
-print(*password)re.sub( ),( )
-
-
+while (number > 3 or number < 20):
+    if (number < 3) or (number > 20):
+        print( )
+        print('Неверное число! Вас раздавили шипы!')
+        break
+    else:
+        print()
+        print('Ваш пароль:')
+        print(*password)
+        break
