4747a76276db87efcb4782aed15ea2b15a8cc15e
Ekaterina Galisheva <masay94@mail.ru>
2024-09-09 12:27:12 +0300

    Delete module_1.5.py

--- module_1.5.py
+++
@@ -1,7 +0,0 @@
-immutable_var=(1, 2.3, 'cat', 'dog')
-print(immutable_var)
-#print(immutable_var[0]=2) #error потому что нельзя изменить кортеж
-mutable_list=[1, 2.3, 5, 'apple', 'Loki']
-mutable_list[2]='frog'
-mutable_list.remove('Loki')
-print(mutable_list)
