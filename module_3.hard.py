7c5976f2c00dec3e4660c046e33f26d5123c8dd7
Ekaterina Galisheva <masay94@mail.ru>
2024-09-09 11:02:40 +0300

    module_3.hard.py

--- module_3.hard.py
+++ module_3.hard.py
@@ -10,28 +10,19 @@ def calculate_structure_sum (data_structure):
     num = 0
     if isinstance(data_structure, str):
         num += int(len(data_structure))
-    if isinstance(data_structure, int, float):
+    elif isinstance(data_structure, (int, float)):
         num += data_structure
-        # здесь нужно закольцевать функцию на типы str или int/float?
-    if isinstance(data_structure, list, tuple, set):
+    elif isinstance(data_structure, (list, tuple, set)):
         for i in data_structure:
-            if i == int or float:
-                num += i
-            if i == str:
-                num += int(len(i))
-            # num += calculate_structure_sum(i)
-    if isinstance(data_structure, dict):
+            num += calculate_structure_sum(i)
+    elif isinstance(data_structure, dict):
         for key, value in data_structure.items():
-            if i == int or float:
-                num += i
-            if i == str:
-                num += int(len(i))
-            #num += calculate_structure_sum(key)
-            #num += calculate_structure_sum(value)
+            num += calculate_structure_sum(key)
+            num += calculate_structure_sum(value)
     return num
 
 
 
 
-finish = calculate_structure_sum(*data_structure)
+finish = calculate_structure_sum(data_structure)
 print(finish)
