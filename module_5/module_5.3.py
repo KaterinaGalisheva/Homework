d76bd329f6476b59cceca22647c4fb8619bbcab0
Ekaterina Galisheva <masay94@mail.ru>
2024-09-10 15:03:47 +0300

    Add files via upload

---
+++ module_5/module_5.3.py
@@ -0,0 +1,76 @@
+# Задача "Нужно больше этажей":
+# Для решения этой задачи будем пользоваться решением к предыдущей задаче "Специальные методы класса".
+#
+# Необходимо дополнить класс House следующими специальными методами:
+# __eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+# Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+# __add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+# __radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+
+class House :
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def __len__(self):
+        return self.number_of_floors
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors == other.number_of_floors
+    def __it__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors <= other.number_of_floors
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors > other.number_of_floors
+    def __ge__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors >= other.number_of_floors
+    def __ne__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors != other.number_of_floors
+    def __add__(self, value):
+        if isinstance(value, int):
+            return self.number_of_floors
+    def __radd__(self, value):
+        if isinstance(value, int):
+            return self.number_of_floors
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            return self.number_of_floors
+
+    def go_to (self, new_floor):
+        if self.number_of_floors >= new_floor > 0:
+            for i in range (1, new_floor + 1):
+                print(i)
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не сужществует!')
+        else:
+            pass
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
