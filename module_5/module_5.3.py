989315a5576baf7dc1d439a419a6a5bcb7b8b5a0
Ekaterina Galisheva <masay94@mail.ru>
2024-09-13 16:51:44 +0300

    module_5.3.py

--- module_5/module_5.3.py
+++ module_5/module_5.3.py
@@ -13,33 +13,30 @@ class House :
         self.number_of_floors = number_of_floors
     def __len__(self):
         return self.number_of_floors
+    def __str__(self):
+        return f'Название: {self.name} количество этажей: {self.number_of_floors}'
     def __eq__(self, other):
-        if isinstance(other, int):
             return self.number_of_floors == other.number_of_floors
     def __it__(self, other):
         if isinstance(other, int):
             return self.number_of_floors < other.number_of_floors
     def __le__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors <= other.number_of_floors
+        return self.number_of_floors <= other.number_of_floors
     def __gt__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors > other.number_of_floors
+        return self.number_of_floors > other.number_of_floors
     def __ge__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors >= other.number_of_floors
+        return self.number_of_floors >= other.number_of_floors
     def __ne__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors != other.number_of_floors
+        return self.number_of_floors != other.number_of_floors
     def __add__(self, value):
         if isinstance(value, int):
-            return self.number_of_floors
+            return House(self.name, self.number_of_floors + value)
     def __radd__(self, value):
         if isinstance(value, int):
-            return self.number_of_floors
+            return House(self.name, self.number_of_floors + value)
     def __iadd__(self, value):
         if isinstance(value, int):
-            return self.number_of_floors
+            return House(self.name, self.number_of_floors + value)
 
     def go_to (self, new_floor):
         if self.number_of_floors >= new_floor > 0:
