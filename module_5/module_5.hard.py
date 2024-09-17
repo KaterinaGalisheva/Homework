9f088cc7ba08bebbf25ffa6ce8761aeea4a833ae
Ekaterina Galisheva <masay94@mail.ru>
2024-09-17 12:08:26 +0300

    module_5.hard.py

--- module_5/module_5.hard.py
+++ module_5/module_5.hard.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import time
 
 class User:
     """
@@ -8,6 +8,12 @@ class User:
         self.nickname = nickname
         self.password = hash(password)
         self.age = age
+
+    def __str__(self):
+        return f"{self.nickname}"
+
+    def __eq__(self, other):
+        return self.title.lower() == other.title.lower()
 
 
 class Video:
@@ -20,7 +26,10 @@ class Video:
         self.time_now = time_now
         self.adult_mode = adult_mode
         if adult_mode >= 18:
-            adult_mode = True
+            self.adult_mode = True
+
+    def __str__(self):
+        return f"Video(title='{self.title}', duration={self.duration}, adult_mode={self.adult_mode})"
 
 class UrTube:
     """
@@ -34,60 +43,64 @@ class UrTube:
 
     def register (self, nickname:str, password:int, age:int):
         for user in self.users:
-            if nickname == user.nickname:
-                print(f'Пользователь {nickname} уже существует')
+            if nickname != user.nickname:
+                new_user = User(nickname, password, age)
+                self.users.append(new_user)
+                self.current_user = new_user
+                print(f'Пользователь {nickname} успешно зарегистрирован и вошёл в систему')
             else:
-                user = User(nickname, password, age)
-                self.users.append(user)
-                break
+                print(f'Пользователь {nickname} уже существует')
 
     def log_in (self, nickname, password):
         for user in self.users:
             if nickname == user.nickname and hash(password) == user.password:
                 self.current_user = user
+                print(f"Пользователь {nickname} вошёл в систему.")
+            else:
+                print("Ошибка входа: неверный логин или пароль.")
 
     def log_out (self):
         self.current_user = None
 
     def add (self, *args):
-        for video in self.videos:
-            if args in video.title:
-                continue
-            else:
-                video = Video(*args)
-                self.videos.append(video)
+        for i in args:
+            if i not in self.videos:
+                self.videos.append(args)
 
     def get_videos (self, word:str):
-        word = word.lower
+        lower_word = word.lower()
         for video in self.videos:
-            if word.lower() in video.title.lower():
+            if lower_word in video:
                 return video.title
             else:
                 print('Такого видео не существует')
 
     def watch_video (self, movie:str):
-        for video in self.videos:
-            if movie in video.title:
-                for i in range(1, 11):
-                    print(i, end=' ')
-                    time.sleep(1)
-                print('Конец видео')
-            elif self.current_user.age < 18:
-                print("Вам нет 18 лет, пожалуйста покиньте страницу")
-                return
-            else:
-                print('Войдите в аккаунт, чтобы смотреть видео')
+        if self.current_user is None:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+        if Video.self.adult_mode > 18 and self.current_user.age < 18:
+            print("Вам нет 18 лет, пожалуйста покиньте страницу")
+            return
+        for second in range(video.time_now + 1, video.duration + 1):
+            print(second, end=' ', flush=True)
+            time.sleep(1)
+            print("Конец видео")
+            video.time_now = 0
+            return
+
+
 
 
 ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
+v1 = Video('лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
 # Добавление видео
 ur.add(v1, v2)
 
 # Проверка поиска
-print(ur.get_videos('лучший'))
+print(ur.get_videos('Лучший'))
 print(ur.get_videos('ПРОГ'))
 
 # Проверка на вход пользователя и возрастное ограничение
