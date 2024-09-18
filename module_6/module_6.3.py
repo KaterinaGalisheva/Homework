f4697c8ca8a8db26526d2612c8cc5e113618b36f
Ekaterina Galisheva <masay94@mail.ru>
2024-09-18 15:57:59 +0300

    module_6.3.py

--- module_6/module_6.3.py
+++ module_6/module_6.3.py
@@ -2,45 +2,44 @@ class Horse:
     """
     Класс описывающий лошадь
     """
-    x_distance = 0
-    sound = 'Frrr'
-
     def __init__ (self):
-        pass
-
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run (self, dx):
-        Horse.x_distance += dx # увеличение дистанции
-        # или Horse.x_distance.append(dx)
+        self.x_distance += dx # увеличение дистанции
+        return self.x_distance
 
 
 class Eagle:
     """
     Класс описывающий орла
     """
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
-
     def __init__ (self):
-        pass
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly (self, dy):
-        Eagle.y_distance += dy
+        self.y_distance += dy
+        return self.y_distance
 
 
 class Pegasus (Horse, Eagle):
     """
     Класс описывающий пегаса
     """
-    def move (self, dx,dy):
-        super().run(dx)
-        super().fly(dy)
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
+    def move (self, dx, dy):
+        return self.run(dx), self.fly(dy)
 
     def get_pos (self):
-        return (super().x_distance, super().y_distance)
+        return self.x_distance, self.y_distance
 
     def voice (self):
-        print(Eagle().sound)
+        print(self.sound)
 
 
 
