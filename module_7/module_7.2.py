3030b430d71dc40714cb1d2e78fda3f2b74fbef4
Ekaterina Galisheva <masay94@mail.ru>
2024-09-21 17:43:04 +0300

    module_7.2.py

--- module_7/module_7.2.py
+++ module_7/module_7.2.py
@@ -1,22 +1,19 @@
-import io
-from pprint import pprint
-
 def custom_write (file_name, strings):
     # file_name - название файла для записи
     # strings - список строк для записи
     file = open (file_name, 'a', encoding = 'utf-8')
-
     # создаем словарь
     string_num = 0
+    keys = []
     for string in strings:
         file.write(f'{string}\n')
-        string_byte = file.tell() #где-то здесь
+        string_byte = file.tell()
         string_num += 1
         key = (string_num, string_byte)
-        string_positions = {key: string}
+        keys.append(key)
     file.close()
+    string_positions = dict(zip(keys, strings))
     return string_positions
-
 
 
 
