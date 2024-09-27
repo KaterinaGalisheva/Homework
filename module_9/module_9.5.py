c4481aa677ca7a2fcac6a20e39aae8d7638db018
Ekaterina Galisheva <masay94@mail.ru>
2024-09-27 15:01:15 +0300

    Update module_9.5.py

--- module_9/module_9.5.py
+++ module_9/module_9.5.py
@@ -19,27 +19,24 @@ __next__ - метод увеличивающий атрибут pointer на ste
 Создайте несколько объектов класса Iterator и совершите итерации с ними при помощи цикла for.
 '''
 
-
 class StepValueError (ValueError):
     pass
 
 class Iterator:
 
-    def __init__(self, start:int, stop:int, step:int = 1):
-        if self.step == 0:
-            raise StepValueError('Шаг не может быть равен 0')
-        else:
+    def __init__(self, start:int, stop:int, step:int=1):
             self.start = start
             self.stop = stop
             self.step = step
-            self.pointer = start
-        
+            self.pointer = start      
 
     def __iter__(self):
         self.pointer = self.start
         return self
     
     def __next__(self):
+        if self.step == 0:
+            raise StepValueError('Шаг не может быть равен 0')
         if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
             raise StopIteration
         self.pointer += self.step
