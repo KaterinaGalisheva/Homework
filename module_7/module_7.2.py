3f4ac8ff363e00bb592566a7028dd03369a8472c
Ekaterina Galisheva <masay94@mail.ru>
2024-10-24 14:27:24 +0300

    Update module_7.2.py

--- module_7/module_7.2.py
+++ module_7/module_7.2.py
@@ -1,20 +1,33 @@
+#   Задача "Записать и запомнить":
+# Создайте функцию custom_write(file_name, strings), которая принимает аргументы file_name - название файла для записи, strings - список строк для записи.
+# Функция должна:
+# Записывать в файл file_name все строки из списка strings, каждая на новой строке.
+# Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>), а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell() перед записью.
+# Пример полученного словаря:
+# {(1, 0): 'Text for tell.', (2, 16): 'Используйте кодировку utf-8.'}
+# Где:
+# 1, 2 - номера записанных строк.
+# 0, 16 - номера байт, на которых началась запись строк.
+# 'Text for tell.', 'Используйте кодировку utf-8.' - сами строки.
+
+import io
+from pprint import pprint
+
 def custom_write (file_name, strings):
     # file_name - название файла для записи
     # strings - список строк для записи
-    file = open (file_name, 'a', encoding = 'utf-8')
+    with open(file_name, 'a', encoding='utf-8') as file:
     # создаем словарь
-    string_num = 0
-    keys = []
-    for string in strings:
-        file.write(f'{string}\n')
-        string_byte = file.tell()
-        string_num += 1
-        key = (string_num, string_byte)
-        keys.append(key)
-    file.close()
-    string_positions = dict(zip(keys, strings))
-    return string_positions
+        string_positions = {}
+        string_num = 0
+        for string in strings:
+            string_num += 1
+            file.write(f'{string}\n')
+            string_byte = file.tell()
+            key = (string_num, string_byte)
+            string_positions[key] = string
 
+    return string_positions
 
 
 if __name__ == '__main__':
@@ -29,4 +42,3 @@ if __name__ == '__main__':
     result = custom_write('test.txt', info)
     for elem in result.items():
         print(elem)
-
