2da8a964da200396e429faa87b7d0c43d02f648f
Ekaterina Galisheva <masay94@mail.ru>
2024-10-31 16:07:46 +0300

    Add files via upload

---
+++ module_16/module_16_3.py
@@ -0,0 +1,45 @@
+# используем библиотеку, для работы с веб интерфейсами
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+# пишем backend интерфейс
+
+# инициализация приложения
+app = FastAPI
+
+users_db = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get('/users')
+async def get_users() -> dict:
+    return users_db
+
+
+@app.post('/user/{username}/{age}')
+async def create_user(user_id: Annotated[str, Path(min_length=0, max_length=100, description='Enter id', example='15')], 
+                      username: Annotated[str, Path(min_length=5, max_length=15, description='Enter username', example='UrbanUser')], 
+                      age: Annotated[int, Path(ge=18, le=100, description='Enter age', example='24')]) -> str:
+    current_index = str(int(max(users_db, key=int)) +1)
+    users_db[current_index] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is registered'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[str, Path(min_length=0, max_length=100, description='Enter id', example='15')], 
+                      username: Annotated[str, Path(min_length=5, max_length=15, description='Enter username', example='UrbanUser')], 
+                      age: Annotated[int, Path(ge=18, le=100, description='Enter age', example='24')]) -> str:
+    new_user = f'Имя: {username}, возраст: {age}'
+    users_db[user_id] = new_user
+    return f'The user {user_id} is registered'
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: Annotated[str, Path(min_length=0, max_length=100, description='Enter id', example='15')]) -> str:
+    users_db.pop(user_id)
+    return f'user with {user_id} was deleted'
+
+
+
+
+
+
