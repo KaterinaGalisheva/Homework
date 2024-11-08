dedfb81490ae17d8adfa8fcd0df36d0c2b7d3505
Ekaterina Galisheva <masay94@mail.ru>
2024-11-08 17:14:08 +0300

    Update task.py

--- module_17/module_17_1/task.py
+++ module_17/module_17_1/task.py
@@ -1,12 +1,10 @@
 
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
-from app.backend.db_depends import get_db
 from typing import Annotated
 
-from app.models import *
 from sqlalchemy import insert
-from app.schemas import CreateCategory, CreateProduct
+from app.schemas import CreateUser, ApdateUser, CreateTask, UpdateTask
 
 from slugify import slugify
 from sqlalchemy import select
@@ -18,79 +16,21 @@ router = APIRouter(prefix='/task', tags=['task'])
 
 
 @router.get('/')
-async def all_tasks(db: Annotated[Session, Depends(get_db)]):
-    products = db.scalars(select(Product).where(Product.is_active == True, Product.stock > 0)).all()
-    if products is None:
-        return HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='There are no product'
-        )
-    return products
-
+async def all_tasks():
+    pass
 
 @router.get('/task_id')
-async def task_by_id(db: Annotated[Session, Depends(get_db)]):
-    products = db.scalars(select(Product).where(Product.is_active == True, Product.stock > 0)).all()
-    if products is None:
-        return HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='There are no product'
-        )
-    return products
+async def task_by_id():
+    pass
 
 @router.post('/create')
-async def create_task(db: Annotated[Session, Depends(get_db)], create_product: CreateProduct):
-    db.execute(insert(Product).values(name=create_product.name,
-                                      description=create_product.description,
-                                      price=create_product.price,
-                                      image_url=create_product.image_url,
-                                      stock=create_product.stock,
-                                      category_id=create_product.category,
-                                      rating=0.0,
-                                      slug=slugify(create_product.name)))
-    db.commit()
-    return {
-        'status_code': status.HTTP_201_CREATED,
-        'transaction': 'Successful'
-    }
-
+async def create_task():
+    pass
 
 @router.put('/update')
-async def update_task(db: Annotated[Session, Depends(get_db)], product_slug: str,
-                         update_product_model: CreateProduct):
-    product_update = db.scalar(select(Product).where(Product.slug == product_slug))
-    if product_update is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='There is no product found'
-        )
-
-    db.execute(update(Product).where(Product.slug == product_slug)
-               .values(name=update_product_model.name,
-                       description=update_product_model.description,
-                       price=update_product_model.price,
-                       image_url=update_product_model.image_url,
-                       stock=update_product_model.stock,
-                       category_id=update_product_model.category,
-                       slug=slugify(update_product_model.name)))
-    db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Product update is successful'
-    }
-
+async def update_task():
+    pass
 
 @router.delete('/delete')
-async def delete_tasks(db: Annotated[Session, Depends(get_db)], product_id: int):
-    product_delete = db.scalar(select(Product).where(Product.id == product_id))
-    if product_delete is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='There is no product found'
-        )
-    db.execute(update(Product).where(Product.id == product_id).values(is_active=False))
-    db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Product delete is successful'
-    }
+async def delete_tasks():
+    pass
