67aac1bd24c55d4a0878f1ee7fd2cb8157ef3187
Ekaterina Galisheva <masay94@mail.ru>
2024-11-27 14:55:58 +0300

    Update module_8.2.py

--- module_8/module_8.2.py
+++ module_8/module_8.2.py
@@ -19,7 +19,7 @@
 
 
 
-def personal_sum(*numbers):
+def personal_sum(numbers):
     result = 0
     incorrect_data = 0
     for i in numbers:
@@ -27,7 +27,7 @@ def personal_sum(*numbers):
             result += i
         except TypeError:
             incorrect_data += 1
-            print(f'Некорректный тип данных для подсчёта суммы: {i}')
+            print(f'Некорректный тип данных для подсчёта суммы: - {i}')
     
     return (result, incorrect_data)
 
@@ -35,9 +35,6 @@ def personal_sum(*numbers):
 
 def calculate_average(numbers):
     try:
-        if not isinstance(numbers, (list, tuple)):
-            raise TypeError
-        
         sum_data, incorrect_data = personal_sum(numbers)
         count_data = len(numbers) - incorrect_data
         
@@ -50,6 +47,7 @@ def calculate_average(numbers):
         return 0
     except TypeError:
         print('В numbers записан некорректный тип данных')
+        return None
 
 
 
@@ -57,10 +55,6 @@ print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка п
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
-
-
-
-
 
 
 
